e4ed7990a0a9b4a7d4957a289699fd0d4c033519
RyoT0325 <132997744+RyoT0325@users.noreply.github.com>
2023-06-30 09:52:18 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+// １行目に記載している　'use strict' は削除しないでください
+
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", () => {
+    document.getElementById("lightOrange").innerText = "😄";
+})
+
+// function test() {
+//     document.getElementById("test").innerHTML = "<img src="https://cdn.pixabay.com/photo/2023/05/25/21/00/bird-8018305_1280.jpg" alt="">";
+// }
+
